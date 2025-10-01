b38cd0e1c5eef41960b7ee4e5b64337fa8e60fe5
TheGreatAdil <adildoesart@gmail.com>
2025-10-01 20:09:32 +0530

    Updated redirect from onboarding

--- app/(auth)/onboarding/layout.tsx
+++ app/(auth)/onboarding/layout.tsx
@@ -7,7 +7,7 @@ export default async function RootLayout({
   children: React.ReactNode;
 }) {
   if ((await auth()).sessionClaims?.metadata.onboardingComplete === true) {
-    redirect("/leaderboard");
+    redirect("/arena");
   }
 
   return <>{children}</>;
